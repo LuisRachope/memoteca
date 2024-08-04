56727020cdff5bd0ab7291ea50b1fc922da22961
Luis Rachope <luis.chiasso@hotmail.com>
2024-08-04 17:15:03 -0300

    Criando uma variação com o ngIf para mostrar uma mensangem quando não houver pensamentos

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ListarPensamentoComponent implements OnInit {
 
   listaPensamentos = [
-    {
+    /*{
       conteudo: 'Passo informações para o componente filho',
       autoria: 'Componente pai',
       modelo: 'modelo3'
@@ -17,7 +17,7 @@ export class ListarPensamentoComponent implements OnInit {
       conteudo: 'Minha propriedade é decorada com @input()',
       autoria: 'Componente filho',
       modelo: 'modelo2'
-    }
+    }*/
   ]
   constructor() { }
 
