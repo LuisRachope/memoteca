81b4f3c6f6d213b9360b3905e5c8da262ad7a914
Luis Rachope <luis.chiasso@hotmail.com>
2024-08-06 21:50:32 -0300

    Inclusão e criação das novas funções de getNextId e criar pensamento nas funções de backend

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Pensamento } from './pensamento';
+import { Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +13,30 @@ export class PensamentoService {
   private readonly API = 'http://localhost:3000/pensamentos'
   constructor(private http: HttpClient) { }
 
-  listar() {
+  listar(): Observable<Pensamento[]> {
     return this.http.get<Pensamento[]>(this.API)
+  }
+
+  getNextId(): Observable<number> {
+    return this.http.get<Pensamento[]>(this.API).pipe(
+      map(pensamentos => {
+        const validIds = pensamentos
+          .map(p => p.id)
+          .filter(id => id !== undefined) as number[]; // Filtrar IDs válidos e assegurar que são números
+        const lastId = validIds.length > 0 ? Math.max(...validIds) : 0;
+        return lastId + 1;
+      })
+    );
+  }
+
+  criar(pensamento: Pensamento): Observable<Pensamento> {
+    return this.getNextId().pipe(
+      map(nextId => {
+        pensamento.id = nextId;
+        return pensamento;
+      }),
+      switchMap(pensamentoComId => this.http.post<Pensamento>(this.API, pensamentoComId))
+    );
   }
 
 }
